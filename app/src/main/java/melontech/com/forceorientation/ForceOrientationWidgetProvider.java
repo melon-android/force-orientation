667af9baa1c56b08fcbd42962db37cf949c8ec09
Dean Panayotov <dpanayotov@melontech.com>
2016-02-10 22:01:20 +0200

    Premature optimization: Moving RemoteViews to fields.

--- app/src/main/java/melontech/com/forceorientation/ForceOrientationWidgetProvider.java
+++ app/src/main/java/melontech/com/forceorientation/ForceOrientationWidgetProvider.java
@@ -17,29 +17,26 @@ public class ForceOrientationWidgetProvider extends AppWidgetProvider {
     private static final String ACTION_FORCE_ORIENTATION_AUTO = "com.melontech.ACTION_FORCE_ORIENTATION_AUTO";
     private static final String ACTION_FORCE_ORIENTATION_CCW = "com.melontech.ACTION_FORCE_ORIENTATION_CCW";
 
-    private static PendingIntent pendingIntentCW;
-    private static PendingIntent pendingIntentAuto;
-    private static PendingIntent pendingIntentCCW;
+    RemoteViews remoteViews;
 
-    private void initPendingIntents(Context context){
-        if(pendingIntentCW ==null){
-            Intent intent = new Intent();
-            intent.setAction(ACTION_FORCE_ORIENTATION_AUTO);
-            pendingIntentCW = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_FORCE_ORIENTATION_CW), PendingIntent.FLAG_UPDATE_CURRENT);
-            pendingIntentAuto = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-            pendingIntentCCW = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_FORCE_ORIENTATION_CCW), PendingIntent.FLAG_UPDATE_CURRENT);
+    private void initRemoteViews(Context context){
+        if(remoteViews == null){
+            PendingIntent pendingIntentCW = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_FORCE_ORIENTATION_CW), PendingIntent.FLAG_UPDATE_CURRENT);
+            PendingIntent pendingIntentAuto = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_FORCE_ORIENTATION_AUTO), PendingIntent.FLAG_UPDATE_CURRENT);
+            PendingIntent pendingIntentCCW = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_FORCE_ORIENTATION_CCW), PendingIntent.FLAG_UPDATE_CURRENT);
+
+            remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+            remoteViews.setOnClickPendingIntent(R.id.cw, pendingIntentCW);
+            remoteViews.setOnClickPendingIntent(R.id.auto, pendingIntentAuto);
+            remoteViews.setOnClickPendingIntent(R.id.ccw, pendingIntentCCW);
         }
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        initPendingIntents(context);
+        initRemoteViews(context);
         for(int appWidgetId : appWidgetIds){
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-            views.setOnClickPendingIntent(R.id.cw, pendingIntentCW);
-            views.setOnClickPendingIntent(R.id.auto, pendingIntentAuto);
-            views.setOnClickPendingIntent(R.id.ccw, pendingIntentCCW);
-            appWidgetManager.updateAppWidget(appWidgetId, views);
+            appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
         }
     }
 
