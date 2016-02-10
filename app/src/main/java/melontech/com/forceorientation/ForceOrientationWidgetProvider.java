c3b2dfa85e2cd6ec0405187b158ae5b3690a4b1e
Dean Panayotov <dpanayotov@melontech.com>
2016-02-10 21:48:52 +0200

    Refactored names from left & right to CW & CCW.

--- app/src/main/java/melontech/com/forceorientation/ForceOrientationWidgetProvider.java
+++ app/src/main/java/melontech/com/forceorientation/ForceOrientationWidgetProvider.java
@@ -13,21 +13,21 @@ import android.widget.Toast;
  */
 public class ForceOrientationWidgetProvider extends AppWidgetProvider {
 
-    private static final String ACTION_FORCE_ORIENTATION_LEFT = "com.melontech.ACTION_FORCE_ORIENTATION_LEFT";
+    private static final String ACTION_FORCE_ORIENTATION_CW = "com.melontech.ACTION_FORCE_ORIENTATION_CW";
     private static final String ACTION_FORCE_ORIENTATION_AUTO = "com.melontech.ACTION_FORCE_ORIENTATION_AUTO";
-    private static final String ACTION_FORCE_ORIENTATION_RIGHT = "com.melontech.ACTION_FORCE_ORIENTATION_RIGHT";
+    private static final String ACTION_FORCE_ORIENTATION_CCW = "com.melontech.ACTION_FORCE_ORIENTATION_CCW";
 
-    private static PendingIntent pendingIntentLeft;
+    private static PendingIntent pendingIntentCW;
     private static PendingIntent pendingIntentAuto;
-    private static PendingIntent pendingIntentRigh;
+    private static PendingIntent pendingIntentCCW;
 
     private void initPendingIntents(Context context){
-        if(pendingIntentLeft==null){
+        if(pendingIntentCW ==null){
             Intent intent = new Intent();
             intent.setAction(ACTION_FORCE_ORIENTATION_AUTO);
-            pendingIntentLeft = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_FORCE_ORIENTATION_LEFT), PendingIntent.FLAG_UPDATE_CURRENT);
+            pendingIntentCW = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_FORCE_ORIENTATION_CW), PendingIntent.FLAG_UPDATE_CURRENT);
             pendingIntentAuto = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-            pendingIntentRigh = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_FORCE_ORIENTATION_RIGHT), PendingIntent.FLAG_UPDATE_CURRENT);
+            pendingIntentCCW = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_FORCE_ORIENTATION_CCW), PendingIntent.FLAG_UPDATE_CURRENT);
         }
     }
 
@@ -36,18 +36,18 @@ public class ForceOrientationWidgetProvider extends AppWidgetProvider {
         initPendingIntents(context);
         for(int appWidgetId : appWidgetIds){
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-            views.setOnClickPendingIntent(R.id.left, pendingIntentLeft);
+            views.setOnClickPendingIntent(R.id.cw, pendingIntentCW);
             views.setOnClickPendingIntent(R.id.auto, pendingIntentAuto);
-            views.setOnClickPendingIntent(R.id.right, pendingIntentRigh);
+            views.setOnClickPendingIntent(R.id.ccw, pendingIntentCCW);
             appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (ACTION_FORCE_ORIENTATION_LEFT.equals(intent.getAction()) ||
+        if (ACTION_FORCE_ORIENTATION_CW.equals(intent.getAction()) ||
                 ACTION_FORCE_ORIENTATION_AUTO.equals(intent.getAction()) ||
-                ACTION_FORCE_ORIENTATION_RIGHT.equals(intent.getAction())) {
+                ACTION_FORCE_ORIENTATION_CCW.equals(intent.getAction())) {
             Toast.makeText(context, intent.getAction(), Toast.LENGTH_SHORT).show();
         }
         super.onReceive(context, intent);
